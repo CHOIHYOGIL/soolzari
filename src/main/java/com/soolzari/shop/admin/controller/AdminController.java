d9009a78398e042af3e926fe3b85d444a1e793f7
송히 <dlqnslthdgml@naver.com>
2021-01-10 00:45:16 +0900

    push test

--- src/main/java/com/soolzari/shop/admin/controller/AdminController.java
+++ src/main/java/com/soolzari/shop/admin/controller/AdminController.java
@@ -1,8 +1,12 @@
 package com.soolzari.shop.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
-
+	@RequestMapping("/faq.sool")
+	public String faq() {
+		return "admin/faq";
+	}
 }
---
+++ src/main/java/com/soolzari/shop/admin/model/vo/NoticePage.java
@@ -0,0 +1,5 @@
+package com.soolzari.shop.admin.model.vo;
+
+public class NoticePage {
+	private String page;
+}
