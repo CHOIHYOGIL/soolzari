dcb75b5023ee09398aa142adb2e11f5e3f4f8b72
jinseon <jinseon038@gmail.com>
2021-01-24 15:19:41 +0900

    최종수정!!

--- src/main/java/com/soolzari/shop/common/ClassScheduler.java
+++ src/main/java/com/soolzari/shop/common/ClassScheduler.java
@@ -47,7 +47,7 @@ public class ClassScheduler {
 				MimeMessageHelper helper = new MimeMessageHelper(mm, true, "UTF-8");
 				helper.setFrom("jinseon038@gmail.com"); 
 				helper.setTo(y.getCliEmail()); 
-				helper.setSubject("펀딩 목표 달성!"); 
+				helper.setSubject("펀딩 목표 달성 안내 메일입니다"); 
 				helper.setText("<h3>후원하신 펀딩의 목표가 달성되었습니다!!</h3><br>지금 확인하세요~<br><a href='http://192.168.0.13/'>술자리soolzari로 이동하기</a><br>일주일 내에 미결제 시 자동 취소됩니다.",true);
 				mailSender.send(mm);
 			}
@@ -56,8 +56,29 @@ public class ClassScheduler {
 				MimeMessageHelper helper = new MimeMessageHelper(mm, true, "UTF-8");
 				helper.setFrom("jinseon038@gmail.com"); 
 				helper.setTo(n.getCliEmail()); 
-				helper.setSubject("펀딩 목표 실패!"); 
-				helper.setText("<h3>후원하신 펀딩이 목표금액을 달성하지 못했습니다..</h3><br>새로운 펀딩을 기대해주세요~<br><a href='http://192.168.0.13/'>술자리soolzari로 이동하기</a>",true);
+				helper.setSubject("펀딩 목표 달성 안내 메일입니다"); 
+				helper.setText("<h3>후원하신 펀딩이 목표금액을 달성하지 못했습니다..</h3><br>앞으로 오픈 될 새로운 펀딩을 기대해주세요~<br><a href='http://192.168.0.13/'>술자리soolzari로 이동하기</a>",true);
+				mailSender.send(mm);
+			}
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	//0 0 8 1 * * 매월 1일 오전 8시에 안내메일 전송
+	@Scheduled(cron = "0 0 8 1 * *")
+	public void subscribePayMail() {
+		//구독중인 사람들만 뽑아서 결제하라고 매월1일에 메일전송
+		ArrayList<String> cliEmailList = dao.subscribePayMail();
+		try {
+			for(String cliEmail : cliEmailList) {
+				MimeMessage mm = mailSender.createMimeMessage();
+				MimeMessageHelper helper = new MimeMessageHelper(mm, true, "UTF-8");
+				helper.setFrom("jinseon038@gmail.com"); 
+				helper.setTo(cliEmail); 
+				helper.setSubject("구독 정기결제 안내 메일입니다"); 
+				helper.setText("<h3>구독중이신 상품을 유지해주세요.</h3><br>새로운 구독상품을 확인해보세요<br><a href='http://192.168.0.13/'>술자리soolzari로 이동하기</a>",true);
 				mailSender.send(mm);
 			}
 		} catch (MessagingException e) {
